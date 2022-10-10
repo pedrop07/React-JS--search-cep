241af692bec1ebab64154c22eb73ba897fe94c28
pedro <pedrofulfaro@gmail.com>
2022-10-10 16:14:59 -0300

    refactored code

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,56 @@
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IMaskInput } from 'react-imask'
 import { useState } from 'react'
 import './App.css'
 import api from './services/api'
 
 function App() {
   const [input, setInput] = useState('');
-  const [cep, setCep] = useState({});
+  const [cep, setCep] = useState(null);
 
-  async function handleSearch(){
+  async function handleSearch(e){
+    e.preventDefault();
 
-    if(input === ""){
+    if(input.length < 9){
       alert('Digite um CEP valido');
       return;
     }
 
     try {
-      const response = await api.get(`${input.replace(/[^\d]+/g,'')}/json`);
-      setCep(response.data);
-      console.log(cep);
+      const response = await api.get(`${input}/json`);
+      response.data.hasOwnProperty('erro') ? alert('CEP invalido') : setCep(response.data);
       
     } catch (error) {
       alert('erro ao Buscar CEP');
       setInput("");
     }    
-     
   }
-
 
   return (
     <>
       <div className="main--container">
         <h1>Buscador de CEP</h1>
         <div className="input--container">
-          <input 
-            type="text"
-            value={input}
-            placeholder="Digite o CEP"
-            className='inputCep'
-            onChange={(e) => {
-              setInput(e.target.value);
-            }}
-           />
+          <form onSubmit={handleSearch}>
+            <IMaskInput 
+              mask="00000-000"
+              type="text"
+              value={input}
+              placeholder="Digite o CEP"
+              className='inputCep'
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+            />
 
-          <button className='searchBtn' onClick={handleSearch} >
-            <FontAwesomeIcon icon={faMagnifyingGlass}  />
-          </button>
+            <button className='searchBtn' onClick={handleSearch} >
+              <FontAwesomeIcon icon={faMagnifyingGlass}  />
+            </button>
+          </form>
         </div>
         
-        {Object.keys(cep).length != 0 && (
+        {cep && (
           <div className='result--container'>
               <h2 >CEP: {cep.cep}</h2>
               <h3>Estado: {cep.uf}</h3>
@@ -58,7 +60,6 @@ function App() {
           </div>
         )}
       </div>
-
     </>
   )
 }
